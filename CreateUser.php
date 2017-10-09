551c946bc406015b85ec8eefd71eeedb17c96bf1
Alex Bohlken <alexbohlken@gmail.com>
2017-10-09 14:31:32 -0500

    Finished writing the php file for the Create User interface

--- CreateUser.php
+++ CreateUser.php
@@ -1,5 +1,7 @@
 <?php
 	$mysqli = new mysqli("mysql.eecs.ku.edu", "abohlken", "abohlken", "abohlken");
+	$uName = $_POST["uname"];
+	$duplicateFlag = false;
 
 	/* check connection */
 	if ($mysqli->connect_errno) {
@@ -7,17 +9,46 @@
 	    exit();
 	}
 
-	#$query = "SELECT Name, CountryCode FROM City ORDER by ID DESC LIMIT 50,5";
+	#if uName is empty string, display error message
+	if($uName == "")
+	{
+		echo "<center>Username must not be blank.</center>";
+	}
+	#else attempt to enter it into the database
+	else
+	{
+		#check that uName is distinct from other User_IDs already in database
+		$query = "SELECT User_ID FROM users";
 
-	if ($result = $mysqli->query($query)) {
+		if ($result = $mysqli->query($query)) {
 
-	    /* fetch associative array */
-	    while ($row = $result->fetch_assoc()) {
-		#printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
-	    }
+		    /* fetch associative array */
+		    while ($row = $result->fetch_assoc()) {
+			if($row["User_ID"]==$uName)
+			{
+				$duplicateFlag = true;
+			}
+		    }
 
-	    /* free result set */
-	    $result->free();
+		    /* free result set */
+		    $result->free();
+		}
+		
+		#if there's a match, display error message
+		if($duplicateFlag)
+		{
+			echo "<center>Username already taken. Please try another username.</center>";
+		}
+		#else create new row in database
+		else
+		{
+			$query = "INSERT INTO users(" . $uName . ");";
+
+			if($mysqli->query($query))
+				echo "<center>User successfully created!</center>";
+			else
+				echo "<center>Something went wrong, user unsuccessfully created!</center>";
+		}
 	}
 
 	/* close connection */

85654d88805b4f8bd462c6b1e165187ef4be0c8d
Alex Bohlken <alexbohlken@gmail.com>
2017-10-13 10:37:56 -0500

    Created php file to hopefully construct a table of the selected user's posts

--- ViewUserPosts.php
+++ ViewUserPosts.php
@@ -1,3 +1,35 @@
 <?php
+	echo "<html><body><table>";
 
+	$mysqli = new mysqli("mysql.eecs.ku.edu", "abohlken", "abohlken", "abohlken");
+	$uName = $_POST["uname"];
+
+	/* check connection */
+	if ($mysqli->connect_errno) {
+	    printf("Connect failed: %s\n", $mysqli->connect_error);
+	    exit();
+	}
+	
+	echo "<tr><th>Posts by" . $uName . "</th></tr>";
+	
+	#attempt to obtain all user IDs
+	$query = 'SELECT Content FROM posts WHERE Author_ID=' . $uName;
+
+	if($result = $mysqli->query($query))
+	{
+			/* fetch associative array */
+			while ($row = $result->fetch_assoc()) {
+					echo "<tr><td>" . $row["Content"] . "</td></tr>";
+			}
+
+			/* free result set */
+			$result->free();
+	}
+	else
+		echo "<tr><td>No posts made by this user yet.</td></tr>";
+
+	/* close connection */
+	$mysqli->close();
+	
+	echo "</table></body></html>";
 ?>
